ceb8c08ff4a53f3d6e13005a56a369b98db127e0
elionick <48615870+elionick@users.noreply.github.com>
2019-05-26 23:35:08 +0200

    Update getFunctions.py

--- getFunctions.py
+++ getFunctions.py
@@ -1,7 +1,7 @@
 import datetime
 # gets a choice input
 def getChoiceInput(user_input):
-    if user_input not in ["q", "Q"]:
+    if user_input not in ['q', 'Q']:
         return int(user_input)
     else:
         return user_input
@@ -9,11 +9,11 @@ def getChoiceInput(user_input):
 # Get a certain error message
 def getErrorMessage(error_code):
     errorMessages = {
-    "choice" : "Error! Please enter a valid option.",
-    "email" : "Error! Please enter a valid email.",
+    'choice' : 'Error! Please enter a valid option.',
+    'email' : 'Error! Please enter a valid email.',
     }
     return errorMessages[error_code]
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     pass
