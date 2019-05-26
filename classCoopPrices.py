daf7442ed445bd606aaf45db9d8f22a056f4b5e3
elionick <48615870+elionick@users.noreply.github.com>
2019-05-26 23:28:28 +0200

    Update classCoopPrices.py

--- classCoopPrices.py
+++ classCoopPrices.py
@@ -1,18 +1,19 @@
 from bs4 import BeautifulSoup as soup
 import requests
+import lxml
 
 # Defines a class which connects to coop and scrapes the information
 class CoopScraper(object):
     def __init__(self, product):
         self.query = product
-        self.url = f"https://www.coopathome.ch/de/search/?text={self.query}"
+        self.url = f'https://www.coopathome.ch/de/search/?text={self.query}'
 
     def loadCoopWebsite(self):
         source = requests.get(self.url).text
         return source
 
     def extractProductInfo(self, source):
-        file_soup = soup(source, "lxml")
+        file_soup = soup(source, 'lxml')
 
         product_name_list = []
         product_price_list = []
@@ -20,23 +21,23 @@ class CoopScraper(object):
         
         # For each product field on the website, I retrieve name, brand, and price
         # Done by looking for corresponding HTML tags.
-        for product in file_soup.findAll("li", class_="list-page__item"):
+        for product in file_soup.findAll('li', class_='list-page__item'):
 
             try:
-                product_name = product.find("div", class_="product-item__details").a.text.strip()
+                product_name = product.find('div', class_='product-item__details').a.text.strip()
                 info = product.find(
-                    "dd", class_="product-item__price__value product-item__price__value--weight").findAll("span")
+                    'dd', class_='product-item__price__value product-item__price__value--weight').findAll('span')
                 product_weight = info[2].text.strip()
                 try:
                     product_price = info[3].text
                 except:
                     product_price = info[2].text
 
-                if "/100g" in product_price:
-                    product_price = product_price.split("/")[0]
+                if '/100g' in product_price:
+                    product_price = product_price.split('/')[0]
                     product_price = float(product_price)*10
-                    product_price = format(product_price, ".2f")
-                    product_price = str(product_price) + " / kg"
+                    product_price = format(product_price, '.2f')
+                    product_price = str(product_price) + ' / kg'
 
                 product_name_list.append(product_name)
                 product_weight_list.append(product_weight)
