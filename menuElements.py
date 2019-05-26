ac330a01f84457a87d22649af9ac8596f0444385
elionick <48615870+elionick@users.noreply.github.com>
2019-05-26 23:36:47 +0200

    Update menuElements.py

--- menuElements.py
+++ menuElements.py
@@ -3,5 +3,5 @@ recipeMenu = ['Check details', 'Send me the recipe per email', 'Go back to previ
 chooseMeal = ['Choose a meal to get recipes: e.g. "Carbonara": ']
 detailMenu = ['Send me the recipe per email', 'Send me the shopping-list per email', 'Go back to previous menu']
 numberOfRecipes = ['Optional: How many results do you want? Press enter to skip!: ']
-email = ["Please enter a valid email address: "]
-translator = ["Please enter a word / sentence that you want to translate: "]
+email = ['Please enter a valid email address: ']
+translator = ['Please enter a word / sentence that you want to translate: ']
